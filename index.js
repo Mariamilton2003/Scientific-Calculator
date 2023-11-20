97b6ee44e33ab60bfd826b78d45db0df003e0148
Mariamilton2003 <miltonmaria732@gmail.com>
2023-11-19 21:44:23 -0500

    Complete Project

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+const toggleTheme = () =>{
+    const themeImg = document.querySelector(".img-icon");
+    const themeBtn = document.querySelector(".theme-button");
+    themeBtn.value = themeBtn.value == "dark" ? "light" :"dark";
+    console.log(themeBtn.value);
+    themeImg.src = themeBtn.value == "dark"?"./asset/icons8-sun-24.png":"./asset/icons8-crescent-moon-16.png";
+    console.log(themeImg.src);
+    if(themeBtn.value == "light"){
+        document.querySelector(':root').style.setProperty("--clr-bg","#F6FDC3");
+        document.querySelector(':root').style.setProperty("--cal-clr-bg","#1F1717");
+        document.querySelector(':root').style.setProperty("--clr-shadow","#252525");
+    }
+    if(themeBtn.value == "dark"){
+        document.querySelector(':root').style.setProperty("--clr-bg","#22092C");
+        document.querySelector(':root').style.setProperty("--cal-clr-bg","#F2FFE9");
+        document.querySelector(':root').style.setProperty("--clr-shadow","#F2FFE9");
+
+    }
+}
+
 const disp = document.querySelector(".calculator-display");
 const btns = document.querySelectorAll(".btn");
 
-var expression ;
+var expression ="";
 
 btns.forEach((btn,i)=>{
     btn.addEventListener("click",()=>{
-        if(btn.textContent === "DEL" || btn.textContent === "=") return
+        if(btn.textContent === "DEL" || btn.textContent === "=" || btn.textContent === "AC") return
         disp.value += btn.textContent;
-        expression += btn.value;
+        expression += btn.getAttribute("value").toLocaleLowerCase();
         console.log(expression);
     });
 });
@@ -15,14 +35,48 @@ btns.forEach((btn,i)=>{
 const DeleteFn = () => {
     console.log("delting...");
     disp.value = (disp.value).toString().slice(0,-1);
+    expression = expression.toString().slice(0,-1);
+}
+
+const clearFn = () => {
+    disp.value = "";
+    expression = "";
 }
 
 const evaluateFn = () => {
-    const sinTest = /\b(?:sin)([\s+][?=[0-9]{1,5})/gm;
-    expression = "sin 90".replace(/\b(?:sin)([\s+][?=[0-9]{1,5})/gm,(match,n1,n2)=>{
-        console.log(match);
-        console.log(n1);
-        console.log(n2);
-    })
-    disp.value = eval(disp.value)
+    const regexExpression =/\b(sin|cos|tan|sqrt|log)(\(\d+\))|(sin|cos|tan|sqrt|log)(?:\s*)(\d+)|(\d+)(\^)(\d+)|(π)|(e)(\d+)|()\b/gm;
+    expression = expression.toString().replace(regexExpression, (match, fun1, n1, fun2, n2, n3, fun3, n4, fun4,fun5,n5) => {
+        console.log(match + " " + fun1 + " " + n1 + " " + fun2 + " " + n2 + " " + fun3 + " " + n3+ fun4 + " " + n4+ fun5 + " " + n5);
+        const fun=fun1 || fun2 || fun3 || fun4 ||fun5;
+        const angle= n1 || n2;
+        console.log(fun);
+        if (fun) {
+            switch (fun) {
+                case "sin":
+                    return `Math.sin(${angle})`;
+                case "cos":
+                    return `Math.cos(${angle})`;
+                case "tan":
+                    return `Math.tan(${angle})`;
+                case "sqrt":
+                    return `Math.sqrt(${angle})`;
+                case "log":
+                    return `Math.log10(${angle})`;
+                case "^":
+                    return `Math.pow(${n3},${n4})`;
+                case "π":
+                    return `Math.PI`;
+                case "e":
+                    return `Math.exp(${n5})`
+                default:
+                    return match;
+            }
+        }
+        return match;
+    });
+    try {
+        disp.value = eval(expression);
+    } catch (error) {
+        console.error("Error evaluating expression:", error);
+    }
 }
